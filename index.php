863afdef33ec21e4b3371e2188c15d2b0149acd4
mcanalejas <miguelcana99@gmail.com>
2020-02-19 10:49:12 +0100

    Estructuracion del proyecto

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<?php 
+<?php
 
 
+session_start();
+
+include_once 'model/bd.inc.php';
+include_once 'view/header.inc.php';
+include_once 'view/footer.inc.php';
+include_once 'controller/controller.inc.php';
+include_once 'view/show_view.php';
+
+actualizar_sesion();
+show_header();
+show_menu();
+show_content();
+show_footer();
 
 ?>
