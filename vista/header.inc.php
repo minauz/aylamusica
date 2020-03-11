374cee02289cd467c9eee1c77768c640f8357ab9
mcanalejas <miguelcana99@gmail.com>
2020-03-11 22:45:19 +0100

    Añadido favicon

--- vista/header.inc.php
+++ vista/header.inc.php
@@ -15,6 +15,7 @@ function show_header()
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="vista/css/estilo.css">
+    <link rel="icon" type="image/png" href="vista/imagenes/imagen_inicio.png">
     <title>Aylamusica</title>
 </head>
 
