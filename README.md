3d8132bc1845af6865e712ee39120cfe35823d12
mcanalejas <miguelcana99@gmail.com>
2020-03-19 20:40:49 +0100

    Fin

--- README.md
+++ README.md
@@ -1,26 +1,11 @@
 # Proyecto segunda evaluación de DINT.
 
-- [x] Buscar una canción por una parte del título (cancion ejemplo 'Malamente')
+Para acceder a la pantalla de login hay que accerlo por http://localhost/aylamusica/index.php?admin=login y ahi podremos iniciar sesion con la contraseña del archivo de 'configuracion.json'.
 
-- [ ] Comentar un párrafo de una canción
+##Funciones hechas: 
 
-- [ ] Elegir el idioma entre tres posibles: español, danés y esloveno
+- Buscar canciones.
+- Mostrar cancion.
+- Mostrar parrafos de canción.
 
-- [ ] Enviar por correo (se solicitará) la canción y todos sus comentarios criptografiados. Para criptografiarlos se usará un método esteganográfico (el texto se guarda-disimula dentro de una imagen) 
-
-- [ ] Subir una canción con intérprete(s) y una imagen asociada 
-
-- [ ] Poner la web on/off 
-
-- [ ] Ver el fichero log de la aplicación, en el cuál se verán todos los comentarios realizados, la fecha en que se realizó el comentario y el título de la canción que fue comentada. Este fichero estará guardado en el servidor. El administrador puede consultarlo desde la zona de administración 
-
-- [ ] Importar / exportar todas las canciones (sin comentarios), incluidas las imágenes 
-
-- [ ] Modificar los anuncios de la web. En la web tendremos cuatro anuncios, pero se debe contemplar la posibilidad de que haya más en el futuro. Deben registrarse las visitas a los anuncios. ->
-Correo masivo a nuestros patrocinadores (anuncios). El correo estará formado por texto únicamente 
-
-- [ ] Añadir una palabra al diccionario de insultos. Cuando una persona ponga un insulto, la aplicación la sustituirá por los caracteres *** (configurable) 
-
-- [ ] Cambiar la contraseña de administración. La contraseña tendrá una al menos una mayúscula, una minúscula, un carácter especial y un número 
-
-- [ ] El sistema expulsará al administrador a los 5 minutos (configurable) de inacción 
+Para dichas pruebas, usar la canción 'Malamente'.
--- vista/header.inc.php
+++ vista/header.inc.php
@@ -8,23 +8,24 @@
 function show_header()
 {
     echo '
-<!DOCTYPE html>
-
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="vista/css/estilo.css">
-    <link rel="icon" type="image/png" href="vista/imagenes/imagen_inicio.png">
-    <title>Aylamusica</title>
-</head>
-
-<body class="mx-3">
-<nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="index.php?cmd=inicio">Inicio</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-</nav>
-<h1 class="text-center"><a href="index.php?cmd=inicio">Aylamusica</a></h1>';
-} ?>
+ <!DOCTYPE html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="vista/css/estilo.css">
+        <link rel="icon" type="image/png" href="vista/imagenes/imagen_inicio.png">
+        <title>Aylamusica</title>
+    </head>
+    
+    <body>
+        <nav class="navbar navbar-expand-lg bg-light">
+                <a class="navbar-brand text-dark" href="index.php?cmd=inicio">Inicio</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+        </nav>
+        <h1 class="text-center"><a href="index.php?cmd=inicio">Aylamusica</a></h1>';
+}
+?>
